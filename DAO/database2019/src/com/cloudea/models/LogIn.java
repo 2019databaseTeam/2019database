4363e5f44c42a047d52770f066af35e9c21ec468
Sugariscool <13661238588@163.com>
2019-11-13 18:20:24 +0800

    dao2

--- DAO/database2019/src/com/cloudea/models/LogIn.java
+++ DAO/database2019/src/com/cloudea/models/LogIn.java
@@ -1,5 +1,35 @@
 package com.cloudea.models;
 
 public class LogIn {
+	private int log_id;
+	private int u_id;
+	private String in_time;
+	public LogIn() {
+		super();
+	}
+	public LogIn(int log_id, int u_id, String in_time) {
+		super();
+		this.log_id = log_id;
+		this.u_id = u_id;
+		this.in_time = in_time;
+	}
+	public int getLog_id() {
+		return log_id;
+	}
+	public void setLog_id(int log_id) {
+		this.log_id = log_id;
+	}
+	public int getU_id() {
+		return u_id;
+	}
+	public void setU_id(int u_id) {
+		this.u_id = u_id;
+	}
+	public String getIn_time() {
+		return in_time;
+	}
+	public void setIn_time(String in_time) {
+		this.in_time = in_time;
+	}
 
 }
--- DAO/database2019/src/com/cloudea/models/UserPerm.java
+++ DAO/database2019/src/com/cloudea/models/UserPerm.java
@@ -2,4 +2,29 @@ package com.cloudea.models;
 
 public class UserPerm {
 
+	private int u_id;
+	private int pmt_id;
+	public UserPerm() {
+		super();
+	}
+	public UserPerm(int u_id, int pmt_id) {
+		super();
+		this.u_id = u_id;
+		this.pmt_id = pmt_id;
+	}
+	public int getU_id() {
+		return u_id;
+	}
+	public void setU_id(int u_id) {
+		this.u_id = u_id;
+	}
+	public int getPmt_id() {
+		return pmt_id;
+	}
+	public void setPmt_id(int pmt_id) {
+		this.pmt_id = pmt_id;
+	}
+	
+
+
 }
